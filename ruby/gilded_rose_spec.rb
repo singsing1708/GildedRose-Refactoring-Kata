bf023c8f325a09140ea2823499fd27385def0294
Steven Wong <sing1708@gmail.com>
2020-11-08 12:07:00 +0800

    add test for 1 test case

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,10 +3,12 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+    it "does not change the name, decrease sell_in and decrease quality" do
+      items = [Item.new("foo", 10, 10)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
+      expect(items[0].sell_in).to eq 9
+      expect(items[0].quality).to eq 9
     end
   end
 
