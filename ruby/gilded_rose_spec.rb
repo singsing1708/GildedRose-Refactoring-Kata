9d00d811cd5e18a6bca0904a445a66314e409510
Steven Wong <sing1708@gmail.com>
2020-11-08 12:21:51 +0800

    grouped the test cases

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,38 +3,35 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name, decrease sell_in and decrease quality" do
-      items = [Item.new("foo", 10, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-      expect(items[0].sell_in).to eq 9
-      expect(items[0].quality).to eq 9
-    end
-  end
+    subject { GildedRose.new(items).update_quality() }
+    context 'when the item has sell_in 10 and quality 10' do
+      let(:items) { [Item.new("foo", 10, 10)] }
+      it "does not change the name, decrease sell_in and decrease quality" do
+        subject
+        expect(items[0].name).to eq "foo"
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 9
+      end
 
-  describe "#update_quality" do
-    it "quality will never be negative" do
-      items = [Item.new("foo", 10, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-      expect(items[0].sell_in).to eq 9
-      expect(items[0].quality).to eq 0
+      it "quality will never be negative" do
+        items = [Item.new("foo", 10, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].name).to eq "foo"
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 0
+      end
     end
-  end
 
-  describe "#update_quality" do
-    it "Once the sell by date has passed, Quality degrades twice as fast" do
-      items = [Item.new("foo", 0, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-      expect(items[0].sell_in).to eq -1
-      expect(items[0].quality).to eq 8
-
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-      expect(items[0].sell_in).to eq -2
-      expect(items[0].quality).to eq 6
+    context 'when the item sell by date has passed' do
+      let(:items) { [Item.new("foo", 0, 10)] }
+      it "Quality degrades twice as fast" do
+        subject
+        expect(items[0].name).to eq "foo"
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 8
+      end
     end
+
   end
 
 end
