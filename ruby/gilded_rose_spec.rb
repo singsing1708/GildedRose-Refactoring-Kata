ae5e167991fbc531671e6bc7a6f7b60b76f0166d
Steven Wong <sing1708@gmail.com>
2020-11-08 12:09:14 +0800

    add test case with quality will never be negative

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -12,4 +12,14 @@ describe GildedRose do
     end
   end
 
+  describe "#update_quality" do
+    it "quality will never be negative" do
+      items = [Item.new("foo", 10, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "foo"
+      expect(items[0].sell_in).to eq 9
+      expect(items[0].quality).to eq 0
+    end
+  end
+
 end
