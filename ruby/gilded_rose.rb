c2777481512982b60f53f2a332bd242a61d3f8af
Steven Wong <sing1708@gmail.com>
2020-11-08 13:45:53 +0800

    clean up old codes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -48,55 +48,6 @@ class GildedRose
     item.decrease_sell_in
   end
 
-
-  def old_logic
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes"
-        if item.quality > 0
-          if item.name != "Sulfuras"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes"
-            if item.quality > 0
-              if item.name != "Sulfuras"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
-  end
-
 end
 
 class Item
